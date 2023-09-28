17ff87afaa3057635474abf2d1f3d1be277a7709
Dayan Fajardo <dayanfajardo.6@gmail.com>
2023-09-28 17:07:53 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,4 @@
+
+//refactorizacion de codigo en el main para que sea lo mas facil de leer
+import './src/blackjack'; // modulos son encapsulamiento de codigo que podemos reutilizar
+import './style.css';
