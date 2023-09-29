16d45b09a49d8c6ca7751952b3e5774414be8dda
Dayan Fajardo <dayanfajardo.6@gmail.com>
2023-09-29 16:28:20 -0500

    vite.config updated

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+
+export default defineConfig ({ // defineConfig espera un objeto o para añadir pluggins
+    base: '/js-vite-blackjack/',
+    plugins: []
+
+})
